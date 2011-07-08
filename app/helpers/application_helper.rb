31b25183bf1f2879dac439b7205fe243da76627b
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2011-07-08 14:58:09 -0500

    got the flipboard working by rewiting php code, adding 5 more jquery files, and updating the css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,11 @@ module ApplicationHelper
   def tweet
     @tweet = Twitter::Client.new.user('codeacademy').status.text rescue ""
   end
+  
+  def sponsors
+    @sponsors = [
+     {:name => "new_world_ventures", :description => "The leading Venture Capital Firm in the Midwest", :url => "http://www.newworldvc.com"},
+     {:name => "Obtiva", :description => "Chicago's top Ruby on Rails Software Consultancy", :url => "http://obtiva.com"}
+    ]
+  end
 end
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -248,3 +248,5 @@ $(window).load(function(){
 
 	});
 });
+
+
